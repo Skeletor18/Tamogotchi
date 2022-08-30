d5365fc97d6c6e73d878db0cc48839e156cccd5b
Akhmed <ahmedchahkiev001@gmail.com>
2022-08-30 17:04:51 +0300

    Tamogotchi

---
+++ tamogotchi.js
@@ -0,0 +1,63 @@
+let mealState = ''
+let energyState = ''
+let moodState = ''
+const tamogotchi = {
+    name: 'egg',
+    meal: 1,
+    energy: 2,
+    mood: 4,
+    getStatus(){
+        if (this.meal < 3) {
+            mealState = 'Я голоден'
+        }else{
+            mealState = 'Я не голоден'
+        }
+        if (this.energy < 3) {
+            energyState = 'Я хочу спать'
+        } else {
+            energyState = 'Я не хочу спать'
+        }
+        if (this.mood < 3) {
+            moodState = 'Мне скучно'
+        } else {
+            moodState = 'Мне весело'
+        }
+        if(this.meal <= 0){
+           return console.log(`${this.name} ${"Смерть"}`) 
+        }
+        if (this.energy <= 0) {
+          return console.log(`${this.name} ${"Смерть"}`)
+        }
+        if (this.mood <= 0) {
+          return console.log(`${this.name} ${"Смерть"}`)
+        }
+        console.log(`Имя: ${this.name}, Еда: ${mealState} ${this.meal}, Энергия: ${energyState}  ${this.energy}, Настроение: ${moodState} ${this.mood}`)
+    },
+    setName(name){
+        this.name = name
+    },
+    setEat(){
+        if (this.meal < 5 || this.mood < 5) {
+            this.meal++ 
+            this.mood--
+
+        }
+    },
+    toSleep(){
+      if (this.energy < 5 || this.meal < 5) {
+        this.energy++
+        this.meal--
+      }
+        
+    },
+    toPlay(){
+      if (this.mood < 5 || this.energy < 5) {
+        this.mood++
+        this.energy--
+      }
+        
+    }
+  }
+  
+  tamogotchi.setName('Skeletor')
+  tamogotchi.getStatus()
