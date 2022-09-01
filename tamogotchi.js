820ebe0083882e57b81418ca1ce3cb5f378c6e05
Skeletor18 <101800458+Skeletor18@users.noreply.github.com>
2022-09-01 23:39:24 +0300

    Update tamogotchi.js

--- tamogotchi.js
+++ tamogotchi.js
@@ -1,11 +1,13 @@
 let mealState = ''
 let energyState = ''
 let moodState = ''
+let weight = ''
 const tamogotchi = {
     name: 'egg',
     meal: 1,
     energy: 2,
     mood: 4,
+    theWeight: 2
     getStatus(){
         if (this.meal < 3) {
             mealState = 'Я голоден'
@@ -22,6 +24,11 @@ const tamogotchi = {
         } else {
             moodState = 'Мне весело'
         }
+        if (this.theWeight < 3){
+            weight = 'Я слишком толстый'
+        }else{
+            weight = 'У меня хороший вес'
+        }
         if(this.meal <= 0){
            return console.log(`${this.name} ${"Смерть"}`) 
         }
@@ -31,7 +38,10 @@ const tamogotchi = {
         if (this.mood <= 0) {
           return console.log(`${this.name} ${"Смерть"}`)
         }
-        console.log(`Имя: ${this.name}, Еда: ${mealState} ${this.meal}, Энергия: ${energyState}  ${this.energy}, Настроение: ${moodState} ${this.mood}`)
+        if (this.theWeight <= 0){
+             return console.log(`${this.name} ${"Смерть"}`)
+        }
+        console.log(`Имя: ${this.name}, Еда: ${mealState} ${this.meal}, Энергия: ${energyState}  ${this.energy}, Настроение: ${moodState} ${this.mood} Вес : ${weight} ${this.theWeight}`)
     },
     setName(name){
         this.name = name
@@ -39,6 +49,7 @@ const tamogotchi = {
     setEat(){
         if (this.meal < 5 || this.mood < 5) {
             this.meal++ 
+            this.theWeight++
             this.mood--
 
         }
@@ -54,6 +65,7 @@ const tamogotchi = {
       if (this.mood < 5 || this.energy < 5) {
         this.mood++
         this.energy--
+         this.theWeight-- 
       }
         
     }
